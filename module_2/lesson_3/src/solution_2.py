62d038c77d8ce679fd60685accc0c3a6ba82ecec
Andrey Durakov <drunia2904@mail.ru>
2024-02-01 15:25:37 +0300

    added solution 2 for lesson 3

---
+++ module_2/lesson_3/src/solution_2.py
@@ -0,0 +1,7 @@
+employee_1 = "Света"
+employee_2 = "Олег"
+employee_3 = "Маша"
+employee_4 = "Паша"
+print("В четные дни работают: " + employee_1 + " " + employee_2)
+print()
+print("В нечетные дни работают: " + employee_3 + " " + employee_4)
