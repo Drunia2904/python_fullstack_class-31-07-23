a15c15570d4f8bda983b3ec562e04ea69dea1ec2
Andrey Durakov <drunia2904@mail.ru>
2024-02-02 12:05:56 +0300

    updated solution 2 for lesson 3

--- module_2/lesson_3/src/solution_2.py
+++ module_2/lesson_3/src/solution_2.py
@@ -1,7 +1,8 @@
 employee_1 = "Света"
-employee_2 = "Олег"
-employee_3 = "Маша"
+employee_2 = "Маша"
+employee_3 = "Олег"
 employee_4 = "Паша"
-print("В четные дни работают: " + employee_1 + " " + employee_2)
+all_employees = [employee_1, employee_2, employee_3, employee_4]
+print("В четные дни работают: ", all_employees[::2])
 print()
-print("В нечетные дни работают: " + employee_3 + " " + employee_4)
+print("В нечетные дни работают: ", all_employees[1::2])
